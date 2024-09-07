9bef3cb56762db4ee5216613e715b3559e847c7e
JDB <jdb@JDBs-MacBook-Air.local>
2024-09-07 02:36:26 -0700

    Initial commit after full history rewrite

--- admin.couple.me/src/components/loader/Loader.jsx
+++
@@ -1,9 +0,0 @@
-import { ClipLoader } from "react-spinners";
-
-export default function Loader() {
-  return (
-    <div className="flex justify-center items-center h-full">
-      <ClipLoader color={"#36D7B7"} size={50} />
-    </div>
-  );
-}
